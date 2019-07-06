fb9b1059534ce478c88967e1a391824533d2b566
krawieck <fil.krawczyk@gmail.com>
2019-07-06 14:44:37 +0200

    added fish_prompt function

---
+++ fish_prompt.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+
+function fish_prompt
+    set_color purple
+    set d (string replace $HOME "~" $PWD)
+    if [ $d = "~"]
+        printf "~ "
+    else if [ $d = "/"]
+        echo "/"
+    else
+        echo $d
+    end
+    set_color green
+    echo "➜ "
+    set -e d
+end
